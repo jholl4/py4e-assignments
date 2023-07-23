f36881e3352caf617bb4a6ec17f78c0e490f638a
Joshua Holland <joshua.holland.1@outlook.com>
2023-07-23 13:16:12 -0400

    Fixed the if/else block to only use floats in the conditional.

--- 3.1.py
+++ 3.1.py
@@ -5,10 +5,10 @@ r = float(rate)
 # The overtime pay rate
 otRt = 1.5 * r
 
-if hrs <= 40:
+if h <= 40:
     grossPay = h * r
 else:
-    otHrs = hrs - 40
+    otHrs = h - 40
     otPay = otHrs * otRt
     normPay = r * 40
     grossPay = normPay + otPay
