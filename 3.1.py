9252feef181fc266e1369d21a0e853c02fdbf9ce
Joshua Holland <joshua.holland.1@outlook.com>
2023-07-23 13:14:21 -0400

    Added overtime compensation to 3.1.py

--- 3.1.py
+++ 3.1.py
@@ -2,4 +2,15 @@ hrs = input("Enter Hours:")
 h = float(hrs)
 rate = input("Enter rate:")
 r = float(rate)
-print(h * r)
+# The overtime pay rate
+otRt = 1.5 * r
+
+if hrs <= 40:
+    grossPay = h * r
+else:
+    otHrs = hrs - 40
+    otPay = otHrs * otRt
+    normPay = r * 40
+    grossPay = normPay + otPay
+    
+print(grossPay)
