4dc17835614e1f64e4d8866db3d8d7d900fab9bd
Joshua Holland <joshua.holland.1@outlook.com>
2023-07-25 18:07:09 -0400

    Completed 8.4.py

--- Ch_8/8.4.py
+++ Ch_8/8.4.py
@@ -7,4 +7,9 @@ fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()
 for line in fh:
-    print(line.rstrip())
+    for word in line.strip().split():
+        if word not in lst:
+            lst.append(word)
+    
+lst.sort()
+print(lst)
