05185f5b92212375ca0c298b3bf9f6bbe95e2147
Joshua Holland <joshua.holland.1@outlook.com>
2023-07-27 19:18:04 -0400

    Removed an unused variable

--- Ch_12/socket1.py
+++ Ch_12/socket1.py
@@ -15,7 +15,6 @@ mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
 cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
-dataDecode = ""
 
 while True:
     data = mysock.recv(512)
